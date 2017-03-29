e2e4b0afdfdff2179acaa3ab6226d1d16bca8979
HKUST\xlibb <sylvia.lixh@gmail.com>
2017-03-29 22:33:53 +0800

    Test Ising model

--- rbm_quantum.py
+++ rbm_quantum.py
@@ -13,7 +13,7 @@ from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
 class rbm_classicalsolver:
     """
     This is a VMC method based on Restricted Boltzmann Machine to find quantum many body ground state.
-     It is the Python implementation with Theano on the Science paper Science 355 ,602¨C606 (2017).
+     It is the Python implementation with Theano on the Science paper Science 355 ,602Â¨C606 (2017).
     For the details, please refer to the original paper.
     """
     def __init__(self,
@@ -23,6 +23,7 @@ class rbm_classicalsolver:
                  vbias=None,
                  W_real=None,
                  W_imag=None,
+                 input=None,
                  np_rng=None,
                  theano_rng=None,
                  ):
@@ -51,10 +52,10 @@ class rbm_classicalsolver:
         self.nhidden=nhidden
         if np_rng is None:
             # create a number generator
-            ny_rng = numpy.random.RandomState(1234)
+            np_rng = np.random.RandomState(1234)
 
         if theano_rng is None:
-            theano_rng = RandomStreams(numpy_rng.randint(2 ** 30))
+            theano_rng = RandomStreams(np_rng.randint(2 ** 30))
 
         if W_real is None:
             # W_real is initialized with `initial_Wreal` which is uniformely
@@ -62,11 +63,11 @@ class rbm_classicalsolver:
             # 4*sqrt(6./(n_hidden+n_visible)) the output of uniform if
             # converted using asarray to dtype theano.config.floatX so
             # that the code is runable on GPU
-            initial_Wreal = numpy.asarray(
-                numpy_rng.uniform(
-                    low=-4 * numpy.sqrt(6. / (n_hidden + n_visible)),
-                    high=4 * numpy.sqrt(6. / (n_hidden + n_visible)),
-                    size=(n_visible, n_hidden)
+            initial_Wreal = np.asarray(
+                np_rng.uniform(
+                    low=-4 * np.sqrt(6. / (nhidden + nvisible)),
+                    high=4 * np.sqrt(6. / (nhidden + nvisible)),
+                    size=(nvisible, nhidden)
                 ),
                 dtype=theano.config.floatX
             )
@@ -78,11 +79,11 @@ class rbm_classicalsolver:
             # 4*sqrt(6./(n_hidden+n_visible)) the output of uniform if
             # converted using asarray to dtype theano.config.floatX so
             # that the code is runable on GPU
-            initial_Wimag = numpy.asarray(
-                numpy_rng.uniform(
-                    low=-4 * numpy.sqrt(6. / (n_hidden + n_visible)),
-                    high=4 * numpy.sqrt(6. / (n_hidden + n_visible)),
-                    size=(n_visible, n_hidden)
+            initial_Wimag = np.asarray(
+                np_rng.uniform(
+                    low=-4 * np.sqrt(6. / (nhidden + nvisible)),
+                    high=4 * np.sqrt(6. / (nhidden + nvisible)),
+                    size=(nvisible, nhidden)
                 ),
                 dtype=theano.config.floatX
             )
@@ -92,8 +93,8 @@ class rbm_classicalsolver:
         if hbias is None:
             # create shared variable for hidden units bias
             hbias = theano.shared(
-                value=numpy.zeros(
-                    n_hidden,
+                value=np.zeros(
+                    nhidden,
                     dtype=theano.config.floatX
                 ),
                 name='hbias',
@@ -103,8 +104,8 @@ class rbm_classicalsolver:
         if vbias is None:
             # create shared variable for visible units bias
             vbias = theano.shared(
-                value=numpy.zeros(
-                    n_visible,
+                value=np.zeros(
+                    nvisible,
                     dtype=theano.config.floatX
                 ),
                 name='vbias',
@@ -122,14 +123,17 @@ class rbm_classicalsolver:
         self.hbias = hbias
         self.vbias = vbias
         self.theano_rng = theano_rng
+        self.input=input
         # **** WARNING: It is not a good idea to put things in this list
         # other than shared variables created in this function.
-        self.params = [self.W_real,self.W_imag,self.hbias, self.vbias]
+        #self.params = [self.W_real,self.W_imag,self.hbias, self.vbias]
+        self.params=[self.W_real,self.hbias,self.vbias]
 
 
     def prop_up(self,vis):
         """
-        Probability of Markov chain step for up propagation (from visible samples to hidden samples)
+        Probability of Markov chain step for up propagation (from visible
+        samples to hidden samples)
 
         :param vis: visible samples
         :return pre_activation: for later optimization
@@ -140,26 +144,29 @@ class rbm_classicalsolver:
 
     def sample_h_givenv(self,v0_sample):
         """
-        Markov chain transition from given visible layer configuration to generate hidden layer configuration
+        Markov chain transition from given visible layer configuration to 
+        generate hidden layer configuration
         :param v0_sample: given visible layer configurations
         :return h1_sample: hidden layer configurations
         """
-        pre_sigmoid_h1,h1_mean = self.propup(v0_sample)
+        
+        pre_sigmoid_h1,h1_mean = self.prop_up(v0_sample)
         # get a sample of the hiddens given their activation
         # Note that theano_rng.binomial returns a symbolic sample of dtype
         # int64 by default. If we want to keep our computations in floatX
         # for the GPU we need to specify to return the dtype floatX
-        h1_sample=theano_rng.binomial(size=h1_mean.shape,
+        h1_sample=2*self.theano_rng.binomial(size=h1_mean.shape,
                                       n=1,
                                       p=h1_mean,
                                       dtype=theano.config.floatX
-                                      )
+                                      )-1
 
         return [pre_sigmoid_h1,h1_mean,h1_sample]
 
     def prop_dn(self,hid):
         """
-        Probability of Markov chain step for up propagation (from visible samples to hidden samples)
+        Probability of Markov chain step for up propagation 
+        (from visible samples to hidden samples)
 
         :param hid: hidden samples
         :return pre_activation: for later optimization
@@ -172,32 +179,35 @@ class rbm_classicalsolver:
 
     def sample_v_givenh(self,h0_sample):
         """
-        Markov chain transition from given hidden layer configuration to generate visible layer configuration
+        Markov chain transition from given hidden layer configuration
+        to generate visible layer configuration
         :param h0_sample: given visible layer configurations
         :return v1_sample: hidden layer configurations
         """
-        pre_sigmoid_v1,v1_mean = self.propdn(h0_sample)
+        pre_sigmoid_v1,v1_mean = self.prop_dn(h0_sample)
         # get a sample of the hiddens given their activation
         # Note that theano_rng.binomial returns a symbolic sample of dtype
         # int64 by default. If we want to keep our computations in floatX
         # for the GPU we need to specify to return the dtype floatX
-        v1_sample=theano_rng.binomial(size=v1_mean.shape,
+        v1_sample=2*self.theano_rng.binomial(size=v1_mean.shape,
                                       n=1,
                                       p=v1_mean,
                                       dtype=theano.config.floatX
-                                      )
+                                      )-1
         return [pre_sigmoid_v1,v1_mean,v1_sample]
 
 
     def gibbs_vhv(self,v0_sample):
         """
-        Markov chain transition of visible sample->hidden sample->visible sample with Gibbs sampling
+        Markov chain transition of visible sample->hidden sample->visible 
+        sample with Gibbs sampling
         :param v0_sample: given visible layer configuration
-        :return v1_sample: visible layer configuration after two steps of Markov chain transition
+        :return v1_sample: visible layer configuration after two 
+        steps of Markov chain transition
         """
         pre_sigmoid_h1,h1_mean,h1_sample=self.sample_h_givenv(v0_sample)
 
-        pre_sigmpid_v1,v1_mean,v1_sample=self.sample_v_givenh(h1_sample)
+        pre_sigmoid_v1,v1_mean,v1_sample=self.sample_v_givenh(h1_sample)
 
         return [pre_sigmoid_v1, v1_mean, v1_sample,
                 pre_sigmoid_h1, h1_mean, h1_sample]
@@ -205,9 +215,11 @@ class rbm_classicalsolver:
 
     def gibbs_hvh(self,h0_sample):
         """
-        Markov chain transition of hidden sample->visible sample->hidden sample, with Gibbs sampling
+        Markov chain transition of hidden sample->visible sample->hidden 
+        sample, with Gibbs sampling
         :param h0_sample: given hidden layer configuration
-        :return h1_sample: hidden layer configuration after two steps of Markov chain transition
+        :return h1_sample: hidden layer configuration after two steps of
+        Markov chain transition
         """
         pre_sigmoid_v1,v1_mean,v1_sample=self.sample_v_givenh(h0_sample)
 
@@ -224,8 +236,8 @@ class rbm_classicalsolver:
         is hf=h/2(sp_i+sm_i), therefore flip each site contribute the same energy h/2,
         but the Ts's is different, but one can sum up Ts's for all s'
          :param v_sample: one sample of the visible layer
-         :param Hamiltonian: Hamiltonian of the physical system we concern, we mainly use
-                Hamiltonian.h
+         :param Hamiltonian: Hamiltonian of the physical system we concern, 
+         we mainly use Hamiltonian.h
          :pbc: periodic boundary condition, 1:periodic, 0: open
 
         """
@@ -233,32 +245,59 @@ class rbm_classicalsolver:
         # vector of nvisible, so ones needs to transpose self.W_real to make it broadcastable
         exponent=-2*v_sample*self.vbias+\
                  T.sum(
-                     T.log(T.cosh(self.hbias-self.W_real.T*v_sample+1j*self.W_imag.T*v_sample)),axis=1)-\
+                     T.log(T.cosh(self.hbias-self.W_real*v_sample)),axis=1)-\
                  T.sum(
-                     T.log(T.cosh(self.hbias+self.W_real.T*v_sample-1j*self.W_imag.T*v_sample),axis=1)
-                 )
+                     T.log(T.cosh(self.hbias+self.W_real*v_sample)),axis=1)
         return T.sum(T.exp(exponent))
+        
+    def changing_weight2(self,v_sample):
+        """
+        function to compute the transition probability flipping spins for v_sample,
+        which is Ts's=conj(psi(s',M))/conj(psi(s,M)),
+        as for transverse field Ising model the flipping term in the Hamiltonian
+        is hf=h/2(sp_i+sm_i), therefore flip each site contribute the same energy h/2,
+        but the Ts's is different, but one can sum up Ts's for all s'
+         :param v_sample: one sample of the visible layer
+         :param Hamiltonian: Hamiltonian of the physical system we concern, 
+         we mainly use Hamiltonian.h
+         :pbc: periodic boundary condition, 1:periodic, 0: open
+
+        """
+        # As self.W_real has the size of nvisible*nhidden, and here v_sample is a
+        # vector of nvisible, so ones needs to transpose self.W_real to make it broadcastable
+        exponent=-2*v_sample*self.vbias+\
+                 T.sum(
+                     T.log(T.cosh(self.hbias-T.shape_padaxis(self.W_real,axis=0)*T.shape_padaxis(v_sample,axis=-1))),axis=2)-\
+                 T.sum(
+                     T.log(T.cosh(self.hbias+T.shape_padaxis(self.W_real,axis=0)*T.shape_padaxis(v_sample,axis=-1))),axis=2)
+                
+        return T.mean(T.sum(T.exp(exponent),axis=1))
+
 
 
 
     def variational_energy(self,v1_sample,Hamiltonian):
         """
-        function to compute flpping energy for one sample to the given sample, which is sum_s'<s|H|s'>
+        function to compute flpping energy for one sample to the given sample, 
+        which is sum_s'<s|H|s'>
          :param v_sample: sample of the visible layer
          :param Hamiltonian: Hamiltonian of the physical system we concern
         """
         # For transverse field Ising model, H= -s_i^s_j+h/2(sp_i+sm_i)
         #periodic boundary condition
-        left_shift_sample=theano.map(lambda x:T.concatenate(x[1:],T.shape_padright(Hamiltonian.pdc*x[0]),axis=1),
-                                   sequences=v1_sample)
+        left_shift_sample,_=theano.map(lambda x:T.concatenate([x[1:],T.stack(Hamiltonian.pbc*x[0])]),
+                                   sequences=[v1_sample])
 
 
         ess=T.sum(T.mean(-left_shift_sample*v1_sample,axis=0))
-        changing_weight=theano.map(fn=self.changing_weight,sequences=v1_sample)
-        esp=1/2*Hamiltonian.h*T.mean(changing_weight)
+        #changingweight,_=theano.map(lambda x: self.changing_weight(x),sequences=[v1_sample])
+        
+        #esp=1/2*Hamiltonian.hfield*T.mean(changingweight)
+        esp=1/2*Hamiltonian.hfield*self.changing_weight2(v1_sample)
         return ess+esp
 
-    def variational_update(self,Hamiltonian,lr=0.1,persisitent=None,k=1,):
+
+    def variational_update(self,Hamiltonian,lr=0.1,persistent=None,k=1):
         """
         This is to update the Neural Network parameters with stochastic gradient descent method.
         :param lr: learning rate used to train the RBM
@@ -275,7 +314,7 @@ class rbm_classicalsolver:
         chain, if one is used.
 
         """
-        pre_sigmoid_ph, ph_mean, ph_sample = self.sample_h_given_v(self.input)
+        pre_sigmoid_ph, ph_mean, ph_sample = self.sample_h_givenv(self.input)
 
         # decide how to initialize persistent chain:
         # for CD, we use the newly generate hidden sample
@@ -291,6 +330,7 @@ class rbm_classicalsolver:
         # Read Theano tutorial on scan for more information :
         # http://deeplearning.net/software/theano/library/scan.html
         # the scan will return the entire Gibbs chain
+        
         (
             [
                 pre_sigmoid_nvs,
@@ -315,6 +355,7 @@ class rbm_classicalsolver:
         # note that we only need the sample at the end of the chain
         chain_end = nv_samples[-1]
         variational=self.variational_energy(chain_end,Hamiltonian)
+        sz=self.magnetization(chain_end)
         # We must not compute the gradient through the gibbs sampling
         gparams = T.grad(variational, self.params, consider_constant=[chain_end])
         # end-snippet-3 start-snippet-4
@@ -325,10 +366,10 @@ class rbm_classicalsolver:
                 lr,
                 dtype=theano.config.floatX
             )
-        if persisitent:
-            updates[persisitent]=nv_samples[-1]
+        #if persistent:
+            #updates[persistent]=nh_samples[-1]
 
-        return updates,variational
+        return updates,variational,sz
 
     def magnetization(self,v1_sample):
         """
@@ -345,71 +386,41 @@ def testRBM_classical():
     hfield=0.1
     nspin=100
     alpha=2
-    n_sample=10000
+    n_sample=1000
     rng = np.random.RandomState(123)
     theano_rng = RandomStreams(rng.randint(2 ** 30))
-    ising1d=Ising1D.Ising1D(nspins,hfield,pbc)
-    initial_sample=RandomStreams.binomial(size=(n_sample,nspin),n=1,p=0.5)
-    rbm_classical=rbm_classicalsolver(nvisible=nspin,nhidden=alpha*nspin,np_rng=rng,theano_rng=theano_rng)
+    ising1d=Ising1D.Ising1D(nspin,hfield)
+    initial_sample=theano.shared(2*rng.binomial(size=(n_sample,nspin),
+                                          n=1,p=0.5).astype('float64')-1,borrow=True)
+    rbm_classical=rbm_classicalsolver(nvisible=nspin,nhidden=alpha*nspin,
+                                      input=initial_sample,
+                                      np_rng=rng,theano_rng=theano_rng)
 
-    updates,variational_energy=rbm_classical.variational_update(ising1d,persisitent=initial_sample,k=15)
+    updates,variational_energy,sz=rbm_classical.variational_update(ising1d,
+                                                                persistent=None,
+                                                                k=1)
     # start-snippet-5
     # it is ok for a theano function to have no output
     # the purpose of train_rbm is solely to update the RBM parameters
     #################################
     #     Training the RBM          #
     #################################
+    index=T.iscalar('index')                                                            
     train_rbm = theano.function(
         [],
-        variational_energy,
+        outputs=[variational_energy,sz],
         updates=updates,
         name='train_rbm'
     )
 
-    times=10
+    times=200
     for epoch in range(times):
         # go through the training set
-        mean_cost = train_rbm()
-        print("For epoch %d , the variational energy is %f ",mean_cost)
+        print(epoch)
+        [mean_cost,sz] = train_rbm()
+        print("For epoch %d , the variational energy is %f, /n magnetization is %f"
+        %(epoch,mean_cost,sz))
 
 
 if __name__=="__main__":
     testRBM_classical()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
